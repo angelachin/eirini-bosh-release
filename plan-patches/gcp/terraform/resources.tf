d3f76bb75f4c82dd1ba2f026df8f52b7429b2243
Angela Chin <achin@pivotal.io>
2020-02-25 11:49:14 -0800

    pin kubernetes provider version to get around bug

--- plan-patches/gcp/terraform/resources.tf
+++ plan-patches/gcp/terraform/resources.tf
@@ -87,6 +87,8 @@ provider "kubernetes" {
   host = "${google_container_cluster.gke-cluster.endpoint}"
   cluster_ca_certificate = "${base64decode(google_container_cluster.gke-cluster.master_auth.0.cluster_ca_certificate)}"
   token = "${data.google_client_config.current.access_token}"
+
+  version = "1.10"
 }
 
 resource "kubernetes_namespace" "system" {
